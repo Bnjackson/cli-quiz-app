f2773959b5688ea78587489f46299fe798d60e33
Benjamin <bnjackson951@gmail.com>
2023-09-23 16:08:45 +0100

    Add functionality to get questions data, start running quiz, and prompt user if they want to play again

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const inputModule = require('./input.js');
 const quizModule = require('./quiz.js');
+const fileHandlingModule = require('./file-handling.js');
 
 console.log(
 `This program is a CLI based quiz. With 4 categories made up of 10 questions each. The categories are:
@@ -12,13 +13,31 @@ Music
 Geography
 
 All questions are multiple choiced. You will gain a point for each question answered correctly and will recieve your score at the end of the quiz.
-Good luck.
-`
-)
+Good luck.`);
 
-function main() {
-    const userCategoryChoice = inputModule.getUserAnswer('What category of questions would you like to choose? ', ["history", "sports", "music", "geography"], "string");
+async function main() {
+    let playAgain = true;
+    //The playAgain variable is changed by the wantToPlayAgain function, while the variable is true the program will continue runnning 
+
+    while (playAgain) {
+        const userCategoryChoice = inputModule.getUserInput('What category of questions would you like to choose? ', ["history", "sports", "music", "geography"]);
+        console.log(`You have chosen ${userCategoryChoice} as your category`);
+
+        const questions = await fileHandlingModule.getQuestionsData(userCategoryChoice);
+        quizModule.runQuiz(questions);
+
+        wantToPlayAgain() ? playAgain = true : playAgain = false;
+    }
+    console.log('Thank you for using this program');
 }
 
+function wantToPlayAgain() {
+    const usersChoice = inputModule.getUserInput('Would you like to play again? y/n ', ['y', 'n']);
+    if (usersChoice === 'y') {
+        return true;
+    } else if (usersChoice === 'n') {
+        return false;
+    }
+}
 
 main()
